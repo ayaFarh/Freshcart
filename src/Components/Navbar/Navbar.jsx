c00747102de294950ce8bf50c61a4f9de51c06f9
ayaFarh <farhaya004@gmail.com>
2024-07-10 00:44:47 +0300

    second upload

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { cartContext } from '../../Context/UserContext/cart.context'
 import { WishlistContext } from '../../Context/WishListcontext/WishList.Context'
 
 export default function Navbar() {
-  const [isVisable,setisVisable]=useState(true)
+  const [isVisable,setisVisable]=useState(false)
   const toggleVisable = ()=>{
     setisVisable(!isVisable)
   }
@@ -15,6 +15,9 @@ export default function Navbar() {
   const {getCartInfo,cartInfo} = useContext(cartContext)
   const {getLogedUserWishList,wishInfo}=useContext(WishlistContext)
 
+  function dispalynav(){
+  setisVisable(false)
+  }
 
   useEffect(()=>{
     getCartInfo()
@@ -23,9 +26,9 @@ export default function Navbar() {
 
   return<>
 
-<nav className='bg-slate-100 fixed z-50 right-0 left-0 '>
-<div className='block  md:flex md:gap-4 xl-flex 2xl:flex justify-center items-center py-4 container'>
-<div className='flex items-center justify-between'>
+<nav className='bg-slate-100 fixed z-50 right-0 left-0'>
+<div className='block  md:flex md:gap-4 xl-flex 2xl:flex justify-between items-center py-4 container'>
+<div className='flex items-center justify-between '>
 <h1 className='xl:mr-8 aling-center' >
     <Link to='' >
         <img src={freashcardImage} alt='' className='w-full'></img>
@@ -33,60 +36,59 @@ export default function Navbar() {
   </h1>
   <i className="fa-solid fa-bars  border-2 py-1 px-2 text-3xl border-black rounded-lg text-black cursor-pointer md:opacity-0 opacity-1 " onClick={toggleVisable}> </i>
 </div>
-  {isVisable && 
-  <>
+
+  <div className={`navbar-menu ${isVisable ? 'block' : 'hidden'} md:flex justify-between items-center gap-2`}>
 
   {token?
   
-  <div className=''>
-<ul className='block md:flex gap-4 my-4 xl:my-0 '>
+  <div className='mr-20 md:flex justify-between items-center gap-10'>
+<ul className='block md:flex gap-4  my-4 xl:my-0 '>
    
     <li className='mb-3'>
         <NavLink className={({isActive})=>{
           return `before:h-[2px]  before:bg-primary relative before:absolute before:left-0 before:-bottom-2 hover:before:w-full  before:transition-width before:duration-300
           hover:font-bold 
           ${isActive ? 'font-bold before:w-full' :""}`
-        }}  to="">Home</NavLink>
+        }}  to="" onClick={dispalynav}>Home</NavLink>
     </li>
     <li className='mb-3'>
         <NavLink className={({isActive})=>{
           return `before:h-[2px] before:bg-primary relative before:absolute before:left-0 before:-bottom-2 hover:before:w-full before:transition-[width]
           hover:font-bold before:duration-300
           ${isActive ? 'font-bold before:w-full' :""}`
-        }} to="products">Products</NavLink>
+        }} to="products" onClick={dispalynav}>Products</NavLink>
     </li>
     <li className='mb-3'>
         <NavLink className={({isActive})=>{
           return `before:h-[2px] before:bg-primary relative before:absolute before:left-0 before:-bottom-2 hover:before:w-full before:transition-[width] 
           hover:font-bold before:duration-300
           ${isActive ? 'font-bold before:w-full ' :""}`
-        }} to="categorys">Categories</NavLink>
+        }} to="categorys" onClick={dispalynav}>Categories</NavLink>
     </li>
     <li className='mb-3'>
         <NavLink className={({isActive})=>{
           return `before:h-[2px]  before:bg-primary relative before:absolute before:left-0 before:-bottom-2 hover:before:w-full before:transition-[width]
           hover:font-bold before:duration-300
           ${isActive ? 'font-bold before:w-full ' :""}`
-        }} to="prands">Brands</NavLink>
+        }} to="prands" onClick={dispalynav}>Brands</NavLink>
     </li>
     <li>
         <NavLink className={({isActive})=>{
           return `before:h-[2px]  before:bg-primary relative before:absolute before:left-0 before:-bottom-2 hover:before:w-full before:transition-[width]
           hover:font-bold before:duration-300
           ${isActive ? 'font-bold before:w-full ' :""}`
-        }} to="allOrders">Orders</NavLink>
+        }} to="allOrders" onClick={dispalynav}>Orders</NavLink>
     </li>
 </ul>
-
-
-</div> :""}
-<Link to="cart" className='ml-auto text-xl relative mr-4'>
+<div><Link to="cart" className='ml-auto text-xl relative mr-6'  onClick={dispalynav}>
     <i className="fa-solid fa-cart-shopping "></i>
     <span className='bg-primary absolute top-0 right-0 w-5 h-5 flex items-center justify-center rounded-full text-xs translate-x-1/2 -translate-y-1/2 font-bold text-white'>{cartInfo === null ? <i className="fa-solid fa-spinner fa-spin"></i>: cartInfo.numOfCartItems || 0 }</span>
 </Link>
-<Link to="wishlist" className='ml-auto text-xl relative '>
+<Link to="wishlist" className='ml-auto text-xl relative '  onClick={dispalynav}>
 <i className="fa-solid fa-heart"></i>
-</Link>
+</Link></div>
+
+</div> :""}
 
 <div className='ml-0 xl:ml-6 md:ml-6  '>
   <ul className='flex  gap-4'>
@@ -120,22 +122,22 @@ export default function Navbar() {
         return `before:h-[2px] before:bg-primary relative before:absolute before:left-0 before:-bottom-2 hover:before:w-full before:transition-[width]
         hover:font-bold before:duration-300
         ${isActive ? 'font-bold before:w-full ' :""}`
-      }} to='/auth/login'>Login</NavLink>
+      }} to='/auth/login' onClick={dispalynav}>Login</NavLink>
     </li>
     <li>
       <NavLink className={({isActive})=>{
         return `before:h-[2px] before:bg-primary relative before:absolute before:left-0 before:-bottom-2 hover:before:w-full before:transition-[width]
         hover:font-bold before:duration-300
         ${isActive ? 'font-bold before:w-full ' :""}`
-      }} to='/auth/signup'>Sign up</NavLink> 
+      }} to='/auth/signup' onClick={dispalynav}>Sign up</NavLink> 
     </li>
   </ul>
 </div>:""}
 
-{token?<div className='mx-3'>
+{token?<div className='mx-3' onClick={dispalynav}>
 <i className="fa-solid fa-right-from-bracket cursor-pointer" onClick={logOut}></i>
 </div>:""}
-  </>}
+  </div>
 </div>
 </nav>
 
